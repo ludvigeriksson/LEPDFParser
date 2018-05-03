3f104a6b8c9686a84f8e8308c06908b1e4e9fc97
Ludvig Eriksson <ludvigeriksson@icloud.com>
2018-05-03 14:26:53 +0200

    Initial Commit

---
+++ LEPDFParser/LEPDFParser.h
@@ -0,0 +1,19 @@
+//
+//  LEPDFParser.h
+//  LEPDFParser
+//
+//  Created by Ludvig Eriksson on 2018-05-03.
+//  Copyright © 2018 Ludvig Eriksson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LEPDFParser.
+FOUNDATION_EXPORT double LEPDFParserVersionNumber;
+
+//! Project version string for LEPDFParser.
+FOUNDATION_EXPORT const unsigned char LEPDFParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LEPDFParser/PublicHeader.h>
+
+
